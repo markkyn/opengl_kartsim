2be7291500d620cbb65cbfedea88829e8464782d
markkyn <marcos.gabriel@ossirian.com>
2024-03-15 02:54:39 -0300

    test file

---
+++ src/test.cpp
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <iostream>
+
+#include "./gameobjects/camera.h"
+
+#define N 4
+#define M 4
+
+int main(int argc, char **argv)
+{
+    Matrix matrix(N, M);
+    Matrix matrix2(N, M);
+
+    for (int i = 0; i < M; i++)
+    {
+
+        matrix.setValue(i, i, 1);
+        matrix2.setValue(i, i, 1);
+    }
+
+    Matrix matrix3 = matrix * matrix2;
+
+    matrix3.print();
+
+    return 0;
+}
